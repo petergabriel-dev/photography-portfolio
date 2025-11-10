1bf1fdbbd3673f12c538df83dd901fa8a69a96f3
petergabriel-dev <lopezpetergabriel@gmail.com>
2025-11-11 00:06:51 +0800

    Moved form from footer to contact me
    Styling for the contact form is currently in progress. I'm having a hard time styling the fucking text fields. Gonna have to look up techniques and methods for styling the forms later on. Fucking bullshit ass.

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,75 +1,16 @@
-import { useState } from "react"
 import '../styles/Footer.css'
 
 function Footer(){
-
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [message, setMessage] = useState('')
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-
-        console.log('Form Submitted:', { email, email, message })
-
-        alert('Message has beent sent! (This is just a demo btw.)')
-
-        setName('')
-        setEmail('')
-        setMessage('')
-    }
-
     return(
         <footer>
             <div>
-                <h3>Contact Me</h3>
+                <h3>Socials</h3>
                 <ul>
                     <li>Facebook</li>
                     <li>Instagram</li>
                     <li>Threads</li>
                     <li>LinkedIn</li>
                 </ul>
-            </div>
-            <div className="form">
-                <h2>Email Me</h2>
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <label htmlFor="name">Name: </label>
-                        <input 
-                            type="text"
-                            id="name"
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                            placeholder="Name"
-                            required
-                        />
-                            
-                    </div>
-                    <div>
-                        <label htmlFor="email">Email: </label>
-                        <input 
-                            type="email"
-                            id="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            placeholder="Email"
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="message">Message: </label>
-                        <input 
-                            type="message"
-                            id="message"
-                            value={message}
-                            onChange={(e) => setMessage(e.target.value)}
-                            placeholder="Type your inquiry here"
-                            required
-                        />
-                    </div>
-
-                    <button type="submit">Send Message</button>
-                </form>
             </div>
         </footer>
     )
